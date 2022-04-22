3b4d102d7593133eab8ada73d4545af61087cf0d
nlunce <97473601+nlunce@users.noreply.github.com>
2022-04-22 15:14:16 -0600

    input demo

--- helloWorld.py
+++ helloWorld.py
@@ -1,1 +1,10 @@
+from tkinter.font import names
+
+
 print('Hello World!')
+name = 'Nathan'
+
+print(name)
+
+name = input('What is your name: ')
+print(name)
---
+++ inputDemo.py
@@ -0,0 +1,12 @@
+favoriteColor = input("What is your favorite color?")
+print('Your favorite color is ' + favoriteColor + '!') 
+
+favoriteNumber = input("What is your favorite number?")
+print('Your favorite number is ' + favoriteNumber + '!') 
+
+favoriteFood = input("What is your favorite food?")
+print('Your favorite food is ' + favoriteFood + '!')
+
+favoriteSport = input("What is your favorite sport?")
+print('Your favorite sport is ' + favoriteSport + '!')
+s
