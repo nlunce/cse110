9853dca287111d79be1a818f75bd72f84e822e68
nlunce <naterrl15@byui.edu>
2022-05-02 17:16:48 -0600

    f

--- 03prepareCheckpoint.py
+++ 03prepareCheckpoint.py
@@ -2,3 +2,15 @@ age = input('What is your age?: ')
 intAge = int(age)
 nextBithday = intAge + 1 
 print(f'On your next birthday, you will be {nextBithday}.' )
+
+eggCartons = input('How many egg cartons do you have?: ')
+intEggCartons = int(eggCartons)
+eggs = intEggCartons * 12
+print(f'You have {eggs} eggs.')
+
+cookies = input('How many cookies do you have?: ')
+intCookie = int(cookies)
+people = input('How many people are there?: ')
+intPeople = int(people)
+divide = intCookie / intPeople
+print(f'Each person may have {divide} cookies.')
