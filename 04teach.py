06c79c399502b595c7cac356882b183454d73f7c
nlunce <naterrl15@byui.edu>
2022-05-09 14:51:00 -0500

    04teach

---
+++ 04teach.py
@@ -0,0 +1,17 @@
+from cmath import sqrt
+import math
+
+m = float(input('Mass (In kg): '))
+g = float(input('Gravity (in m/s^2, 9.8 for Earth, 24 for Jupiter): '))
+t = float(input('Time (in seconds): '))
+p = float(input('Density of the fluid (in kg/m^3, 1.3 for air, 1000 for water): '))
+A = float(input('Cross sectional area (in m^2): '))
+C = float(input('Drag constant (0.5 for sphere, 1.1 for cylinder: '))
+
+#Equations
+
+c = .5 * p * A * C
+
+
+v = (math.sqrt(m*g/c)) * (1 - (math.e)**(-(math.sqrt(m*g*c))/m)*t)
+print(v)
