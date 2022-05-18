d01c97b3612d265809b8cad276f0db9721186be1
nlunce <naterrl15@byui.edu>
2022-05-18 14:49:57 -0500

    05prove

---
+++ 05prove.py
@@ -0,0 +1,8 @@
+import math
+
+decision1 = input('Rowley from the popular book series "Diary of a Wimpy Kid" wakes you up. You are in a dark McDonalds, it is 3:00 in the morning. \nRowley asks you if we should STAY in the McDonalds or LEAVE?\nSTAY or LEAVE: ')
+
+if decision1.lower() == 'leave':
+    decision2 = input('You and Rowley leave the McDonalds, its dark, you see a large homeless man with a baseball bat.\nThe homeless man starts yelling incoherent statements about the Vietnam War and begins to run toward you.\nDo you FIGHT or RUN?\nFIGHT or RUN: ')
+elif decision1.lower() == 'stay'
+    
