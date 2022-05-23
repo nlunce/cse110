0fdccf0fe0bcf17817de3697e53919fd1f134847
nlunce <naterrl15@byui.edu>
2022-05-23 14:37:38 -0500

    0506

--- 05prove.py
+++ 05prove.py
@@ -3,27 +3,24 @@ import math
 
 decision1 = input('Rowley from the popular book series "Diary of a Wimpy Kid" wakes you up. You are in a dark McDonalds, it is 3:00 in the morning. \nRowley asks you if we should STAY in the McDonalds or LEAVE?\nSTAY or LEAVE: ')
 
-if decision1.lower() != 'leave' or decision1.lower() != 'stay':
+while decision1.lower() != 'leave' and decision1.lower() != 'stay':
     decision1 = input('INVALID INPUT\nLEAVE or STAY: ')
         
 
 if decision1.lower() == 'leave':
     decision2 = input('You and Rowley leave the McDonalds, its dark, you see a large homeless man with a baseball bat.\nThe homeless man starts yelling incoherent statements about the Vietnam War and begins to run toward you.\nDo you FIGHT or RUN?\nFIGHT or RUN: ')
     
-    while decision2.lower() != 'fight' or decision2.lower() != 'run':
+    while decision2.lower() != 'fight' and decision2.lower() != 'run':
         decision2 = input('IMPROPER INPUT\nFIGHT or RUN: ')
-        if decision2.lower() == 'fight' or decision2.lower() == 'run':
-            break
-
+        
 elif decision1.lower() == 'stay':
     decision2 = input('You and Rowley stay in the McDonalds till morning. A homeless man knocks on the door to the McDonalds.\nDo you OPEN the door for him or REFUSE to let him in?\nOPEN or REFUSE: ')
        
-    while decision2.lower() != 'open' or decision2.lower() != 'refuse':
+    while decision2.lower() != 'open' and decision2.lower() != 'refuse':
         decision2 = input('IMPROPER INPUT\nOPEN or REFUSE: ')
-        if decision2.lower() == 'open' or decision2.lower() == 'refuse':
-            break
 
-    
+      
+
     
 if decision2.lower() == 'fight':
     print('You underestimated the homeless man and as he gets closer you realize he is a huge brute of a man. You are weaponless where as he has a baseball bat.\nThe homeless man beats you to death.\nGAME OVER')
---
+++ 06teach.py
@@ -0,0 +1,6 @@
+import math
+
+
+age_one = input('What is the age of the first rider?: ')
+height_one = input('What is the height of the first rider?: ')
+second_rider = input('Is there a second rider (yes/no)?: ')
