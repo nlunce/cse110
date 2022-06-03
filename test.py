b4cd10da6dbba6c2cbb5de7a243572e8b006b4ec
nlunce <naterrl15@byui.edu>
2022-06-02 21:20:12 -0500

    07

--- test.py
+++ test.py
@@ -1,29 +1,77 @@
-secret_word = 'fortnite'
-hint = '________'
-list_hint = list(hint)
-list_secret_word = list(secret_word)
+import random
+import math
+
+
+
+
+
+def word_picker_function():
+    secret_word_list = ['fortnite', 'cheese', 'dinosaur', 'chicken', 'pickle', 'computer', 'rainbow', 'remember', 'desktop', 'finger', 'alien']
+    word_picker = random.randint(0,10)
+    secret_word = secret_word_list[word_picker]
+    global list_hint
+    global list_secret_word
+    list_hint = ['_' for q in range(len(secret_word))]
+    list_secret_word = list(secret_word)
+
+
+def game():
+    count = 0
+    global list_guess
+    global guess
+
+    if len(list_guess) > len(list_secret_word):
+        length_of_guess = len(list_guess)
+        add_amount = length_of_guess - len(list_secret_word)
+        add_amount_range = range(add_amount)
+        for i in add_amount_range:
+            list_secret_word.append('_')
+            list_hint.append('_')
+
+
+
+    while list_guess != list_secret_word:
+        for letter in list_guess:
+            if letter == list_secret_word[count]:
+                list_hint[count] = letter.capitalize()
+                count += 1
+            elif letter in list_secret_word:
+                list_hint[count] = letter.lower()
+                count += 1
+            else:
+                count +=1
+        print('Your hint is: ' + ''.join(list_hint))
+        guess = input('What is your guess?: ').lower()
+        list_guess = list(guess)
+
+
+
+
+word_picker_function()
 
 print('Welcome to the word guessing game!')
 
-
-
- 
-
-
-print(f'Your hint is: {hint}')
+print('Your hint is: ' + ''.join(list_hint))
 guess = input('What is your guess?: ').lower()
 list_guess = list(guess)
-count = 0
 
-if len(list_guess) > len(list_secret_word):
-    length_of_guess = len(list_guess)
-    add_amount = length_of_guess - len(list_secret_word)
-    add_amount_range = range(add_amount)
-    for i in add_amount_range:
-        list_hint.append('_')
 
-print(list_hint)
+game()
+    
 
 
 
 
+
+
+
+
+
+
+
+
+
+
+# print('Your hint is: ' + ''.join(list_hint))
+# guess = input('What is your guess?: ').lower()
+# list_guess = list(guess)
